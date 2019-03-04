c9144daff37033b5a79fc22b5e19575998e5498d
Robert Billington <chillingtonbillington@gmail.com>
2019-03-04 10:40:09 -0600

    NavBar created

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import '../css/NavBar.css'
 
 const NavBar = props => (
-    <div>
-        <span>
-            <Link to="/settings">Settings</Link>
-        </span>
-        <span>
-            <Link to="/billing">Billing</Link>
-        </span>
+    <div className = 'Nav-Bar'>
+        <div className = 'nav-logo-container'>
+            <h1 className = 'nav-logo'>kookr</h1>
+        </div>
+        <div className = 'nav-link-container'>
+            <Link className = 'nav-link' to = '/create'>Add Recipe</Link>
+            <Link className = 'nav-link' to="/settings">Settings</Link>
+            <Link className = 'nav-link' to="/billing">Billing</Link>
+        </div>
     </div>
 );
 
