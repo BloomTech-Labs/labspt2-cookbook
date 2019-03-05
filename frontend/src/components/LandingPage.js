d30e0b4368eae65edc56796873056511defb1ca8
Robert Billington <chillingtonbillington@gmail.com>
2019-03-04 18:52:44 -0600

    Reformatted landing page to match partners'

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -6,37 +6,31 @@ class LandingPage extends React.Component{
     //Verifying user login?
     render(){
         return(
-            <div className = 'Landing-Page'>
-                <div className = 'landing-page-nav-bar'>
-                    <div className = 'logo-container'>
-                        <div className = 'icon-border'>
-                            <img className = 'icon' src = 'https://image.flaticon.com/icons/svg/1530/1530648.svg'/>
+            <div className='Landing-Page'>
+                <div className='landing-page-nav-bar'>
+                    <div className='logo-container'>
+                        <div className='icon-border'>
+                            <img className='icon' src='https://image.flaticon.com/icons/svg/1530/1530648.svg' alt = 'whisk icon'/>
                         </div>    
-                        <h1 className = 'landing-header'>kookr
+                        <h1 className='landing-header'>kookr
                         </h1>
                     </div>    
-                    <div className = 'landing-page-nav-buttons'>
-                        <h3 className = 'login-button'>Login</h3>
-                        <h3 className = 'registration-button'>Register</h3>
+                    <div className='landing-page-nav-buttons'>
+                        <h3 className='login-button'>Login</h3>
+                        <h3 className='registration-button'>Register</h3>
                     </div>
                 </div>
-                <div className = 'landing-page-sub-container'>
-                    <div className = 'blurb-outline'>
-                        <div className= 'landing-page-blurb-container'>
-                            <div className = 'blurb'>
-                                <div className = 'blurb-p'>
+                <div className='landing-page-sub-container'>
+                    <div className='blurb-outline'>
+                        <div className='landing-page-blurb-container'>
+                            <div className='blurb'>
+                                <div className='blurb-p'>
                                     Bringing fun and focus back to meal planning.
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>   
-                <div className = 'main-content'>
-                    
-
-                    
-                </div>
-            
             </div>
         )
     }
