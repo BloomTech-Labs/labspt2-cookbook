4d8be6f42678074aadfe5e7a38f3ba4a678e8ebc
shannonhealy <sahealy525@gmail.com>
2019-03-04 22:02:52 -0500

    basic settings form

--- frontend/src/components/Settings.js
+++ frontend/src/components/Settings.js
@@ -6,7 +6,29 @@ class Settings extends Component {
         return (
             <div>
                 <NavBar />
-                Settings Page (Placeholder)
+                <div>
+                    <form>
+                        <label htmlFor="email">Email Address: </label>
+                        <input type="text" name="email" placeholder="someone@website.com"></input>
+
+                        <label htmlFor="phone">Phone Number: </label>
+                        <input type="tel" name="phone" placeholder="555-123-5678"></input>
+
+                        <label htmlFor="notificationsEmail">Email Notifications? </label>
+                        <input type="checkbox" name="notificationsEmail"></input>
+
+                        <label htmlFor="notificationsText">Text Notifications? </label>
+                        <input type="checkbox" name="notificationsText"></input>
+
+                        <label htmlFor="passOld">Old Password: </label>
+                        <input type="password" name="passOld"></input>
+                        
+                        <label htmlFor="passNew">New Password: </label>
+                        <input type="password" name="passNew"></input>
+                        
+                        <input type="submit" name="save" value="Save"></input>
+                    </form>
+                </div>
             </div>
         );
     }
