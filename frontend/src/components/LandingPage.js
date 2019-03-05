63198acd7c6aa4cdc01feee70f7d0bfa61f8d56e
Robert Billington <chillingtonbillington@gmail.com>
2019-03-05 13:46:13 -0600

    Changed some styling on Landing, Nav, and Create

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -8,29 +8,32 @@ class LandingPage extends React.Component{
         return(
             <div className='Landing-Page'>
                 <div className='landing-page-nav-bar'>
-                    <div className='logo-container'>
-                        <div className='icon-border'>
-                            <img className='icon' src='https://image.flaticon.com/icons/svg/1530/1530648.svg' alt = 'whisk icon'/>
+                        <div className='logo-container'>
+                            <div className='icon-border'>
+                                <img className='icon' src='https://image.flaticon.com/icons/svg/1034/1034680.svg' alt = 'logo icon'/>
+                            </div>    
+                            <h1 className='landing-header'>kookr
+                            </h1>
                         </div>    
-                        <h1 className='landing-header'>kookr
-                        </h1>
-                    </div>    
-                    <div className='landing-page-nav-buttons'>
-                        <h3 className='login-button'>Login</h3>
-                        <h3 className='registration-button'>Register</h3>
+                        <div className='landing-page-nav-buttons'>
+                            <h3 className='login-button'>Login</h3>
+                            <h3 className='registration-button'>Register</h3>
+                        </div>
                     </div>
-                </div>
-                <div className='landing-page-sub-container'>
-                    <div className='blurb-outline'>
-                        <div className='landing-page-blurb-container'>
-                            <div className='blurb'>
-                                <div className='blurb-p'>
-                                    Bringing fun and focus back to meal planning.
+                <div className='landing-page-img-container'>
+                    
+                    <div className='landing-page-sub-container'>
+                        <div className='blurb-outline'>
+                            <div className='landing-page-blurb-container'>
+                                <div className='blurb'>
+                                    <div className='blurb-p'>
+                                        Bringing fun and focus back to meal planning.
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </div>   
+                    </div>  
+                </div>     
             </div>
         )
     }
--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -4,11 +4,8 @@ import '../css/NavBar.css'
 
 const NavBar = props => (
     <div className='Nav-Bar'>
-        <div className='nav-logo-container'>
-            <div className='icon-border'>
-                <img className='nav-icon' src='https://image.flaticon.com/icons/svg/1530/1530648.svg' alt = 'whisk icon'/>
-            </div>  
-            <h1 className='nav-logo'>k</h1>
+        <div className='nav-icon-container'>
+            <img className='nav-icon' src='https://image.flaticon.com/icons/svg/1034/1034680.svg' alt = 'logo icon'/>
         </div>
         <div className='nav-link-container'>
             <Link className='nav-link' to='/create'>Add Recipe</Link>
