c3062a7eeb4c786478d80a42ce7939b419dafe83
Christopher Atoki <christopher.atoki@yahoo.com>
2019-03-06 21:26:52 -0500

    Revert "Robs branch"

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -4,18 +4,16 @@ import '../css/NavBar.css'
 
 const NavBar = props => (
     <div className='Nav-Bar'>
-       <div className='nav-link-overlay'>
-            <div className = 'nav-link-container'>
-                <Link className='nav-link' to='/create'>Create</Link>
-                <Link className='nav-link' to="/recipes">Recipes</Link>
-                <Link className='nav-link' to="/grocerylist">Grocery List</Link>
-                <div className='nav-icon-container'>
-                    <img className='nav-icon' src='https://image.flaticon.com/icons/svg/1460/1460085.svg' alt = 'logo icon'/>
-                </div>
-                <Link className='nav-link' to='/calendar'>Calendar</Link>
-                <Link className='nav-link' to="/settings">Settings</Link>
-                <Link className='nav-link' to="/billing">Billing</Link>
-            </div>    
+        <div className='nav-logo-container'>
+            <div className='icon-border'>
+                <img className='nav-icon' src='https://image.flaticon.com/icons/svg/1530/1530648.svg' alt = 'whisk icon'/>
+            </div>  
+            <h1 className='nav-logo'>k</h1>
+        </div>
+        <div className='nav-link-container'>
+            <Link className='nav-link' to='/create'>Add Recipe</Link>
+            <Link className='nav-link' to="/settings">Settings</Link>
+            <Link className='nav-link' to="/billing">Billing</Link>
         </div>
     </div>
 );
