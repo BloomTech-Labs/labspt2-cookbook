2ece7e6d6fddd659b2c5bc66048a55a3127c343c
Jennifer King <jenbellpgh@gmail.com>
2019-03-10 23:48:32 -0400

    Added seed for user_recipes table

--- backend/data/seeds/01_users.js
+++ backend/data/seeds/01_users.js
@@ -14,7 +14,7 @@ exports.seed = async function(knex, Promise) {
   await knex('users').truncate();
 
   const fakeUsers = [];
-  const desiredFakeUsers = process.env.SEEDS || 5;
+  const desiredFakeUsers = process.env.SEEDS || 3;
 
   for( let i = 0; i < desiredFakeUsers; i++ ) {
     fakeUsers.push( createFakeUser() );
