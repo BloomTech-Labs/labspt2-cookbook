d13ef934eb2b19c0a2b280d7d69990965a53d6d7
Denise Escobar <descobar1490@gmail.com>
2019-03-20 23:30:43 -0500

    Created more routes for tags... -Fixed issues with post -created a put route -created a delete route for the recipe_tags table -created a delete route for the tags table

--- backend/data/helpers/tagsModel.js
+++ backend/data/helpers/tagsModel.js
@@ -8,7 +8,7 @@ module.exports = {
  */
 
 getId: function(tag) {
-    return db('tags').where('name', tag).first().pluck('tag_id');
+    return db('tags').where('tag', tag).pluck('tag_id');
 },
 
 
@@ -22,54 +22,113 @@ getByRecipe: function(recipe_id) {
     return db('tags as t').join('recipe_tags as rt', 't.tag_id', 'rt.tag_id').select('t.tag').where('rt.recipe_id', recipe_id)
 },
 
-//get all available meal tags with/without id
+/*
+ * getall: 
+ *
+ *  -- Get all available meal tags with/without id
+ */ 
 
 getAll: function(id) {
     return id ? db('tags').where('tag_id', id).first() : db('tags');
 },
 
-//update meal tags ??
+/*
+ * update: 
+ *
+ *  -- Update tag by id
+ *  -- Returns entire tag object
+ */ 
+update: function(id, change){
+    return db('tags').where('tag_id', id).update(change).then(count => (count > 0 ? this.getAll(id) : null));
+},
 
-//post meal tags to a recipe and also to recipe tags if not there
+/*
+ * insert: 
+ *
+ *  -- Insert new tag to tags table and to recipe_tags table
+ *  -- Returns tag id
+ */ 
+insert: async function(tag, recId) {
+    console.log("in async", tag.tag, recId)
+    const [tagId] = await this.getId(tag.tag)
+    
+    //Check if tag exists
+    if(tagId > 0) {
+        console.log(tagId);
 
-insert: function(tag, recipe_id) {
-    return db.transaction(function(trans){
+        //Tag exists. Check if already part of recipe
+        return db('recipe_tags').where({
+            recipe_id: recId.id,
+            tag_id: tagId
+        }).pluck('id')
+        .then( result =>{
+            if(result.length <=0){
 
-        return db('tags')
-        .transacting(trans)
-        .where('tag', tag).first().pluck('tag_id')
-        .then(([tag_id])=>{
-            if(!tag_id){
-
-                return db('tags')
-                .transacting(trans)
-                .insert({tag: tag})
-                .then( ([id]) => {
-                    return id;
+                //tag not attached to recipe. 
+                return db('recipe_tags')
+                .insert({
+                    recipe_id: recId.id,
+                    tag_id: tagId
                 })
+                .return(tagId);
+            } else{
+
+                //tag is linked
+                return tagId;
             }
-            return tag_id;
-        })
-        .then((tag_id) => {
+        });
+    } else {
+        //tag does not exist
 
-            return db('recipe_tags')
+        return db.transaction( trans =>{
+            return db('tags')
             .transacting(trans)
-            .insert({
-                recipe_id: recipe_id,
-                tag_id: tag_id
-            })
-            .then( ([id]) =>{
-                return id;
-            });
-        })
-        .then(trans.commit)
-        .catch(trans.rollback)
-    })
-    .catch( (err) =>{
-        console.log("Error:", err)
-    })
-}
-//delete meal tags from specific recipe 
+            .insert(tag)
+            .then( result =>{
+                
+                const tag_id=result[0]
 
-//delete meal tag from whole site
+                return db('recipe_tags')
+                .transacting(trans)
+                .insert({
+                    recipe_id: recId.id,
+                    tag_id: tag_id
+                })
+                .return(tag_id)
+            })
+            .then(trans.commit)
+            .catch(trans.rollback)
+        })
+        .then( result =>{
+            return(result);
+        })
+        .catch(function(err) {
+            console.log("error: ", err)
+        })
+    }
+    
+},
+
+/*
+ * remove: 
+ *
+ *  -- Deletes tag from recipe_tags table for a specific recipe
+ *  
+ */ 
+remove: function (id, recipe_id) {
+    return db('recipe_tags').where('recipe_id', recipe_id).andWhere('tag_id', id ).del()
+},
+
+
+/*
+ * totalRemove: 
+ *
+ *  -- Deletes tag from tags table 
+ *  -- Possible TODO: Check recipe_tags table and delete from there as well?
+ */ 
+totalRemove: function(id) {
+    return db('tags').where('tag_id', id).del()
+}
+
+
 };
--- backend/routes/tagsRouter.js
+++ backend/routes/tagsRouter.js
@@ -8,8 +8,7 @@ const tags = require('../data/helpers/tagsModel');
 
 /* ---------- Endpoints for /api/tags ---------- */
 
-//get 
-
+/* GET all tags */
 router.get('/', (req, res) =>{
     
     tags.getAll()
@@ -23,8 +22,7 @@ router.get('/', (req, res) =>{
     })
 })
 
-//get//api/tags/recipe/id
-
+/* GET recipe's tags */
 router.get('/recipe/:id', (req, res) =>{
     const recipe_id = req.params.id;
 
@@ -46,22 +44,74 @@ router.get('/recipe/:id', (req, res) =>{
     })
 })
 
-//post//api/tags/recipe/id
-router.get('/recipe/:id', (req, res) =>{
-    const recipe_id = req.params.id;
+/* PUT */
+router.put('/:id', (req, res) =>{
+    const tag = req.body;
+    const id = req.params.id;
+
+    if(tag.tag){
+        tags.update(id, tag)
+        .then(tag =>{
+            res.json(tag)
+        })
+        .catch(err =>{
+            res.status(500).json({error: "Could not update tag"})
+        })
+    } else {
+        res.status(400).json({error:"Missing data"})
+    }
+    
+})
+
+/* POST */
+router.post('/recipe/:id', (req, res) =>{
+    const recId = req.params;
     const tag = req.body;
 
-    tags.insert(tag, recipe_id)
-    .then(id =>{
-        console.log(id)
-        res.json(id)
-    })
-    .catch(err =>{
-        console.log(err)
-    })
+    if(tag.tag){
+        tags.insert(tag, recId)
+        .then(id =>{
+            res.json(id)
+        })
+        .catch(err =>{
+            res
+            .status(500)
+            .json({error: "Could not add tag to database"})
+        })
+    } else {
+        res.status(400).json({error: "Missing data"});
+    }
+    
 })
 
 
+/* DELETE by recipe id */
+router.delete('/recipe/:id/:tagId', (req, res) =>{
+    const recId = req.params.id;
+    const tagId = req.params.tagId;
+
+    tags.remove(tagId, recId)
+    .then(count =>{
+        res.json(count)
+    })
+    .catch(err =>{
+        res.status(500).json({error:"Could not delete tag"})
+    })
+}) 
+
+/* DELETE from all */
+router.delete('/:id', (req, res) =>{
+    const tagId = req.params.id;
+    console.log(tagId)
+
+    tags.totalRemove(tagId)
+    .then(count =>{
+        res.json(count)
+    })
+    .catch(err =>{
+        res.status(500).json({error:"Could not delete tag"})
+    })
+}) 
 
 
 /* ---------- Export ---------- */
