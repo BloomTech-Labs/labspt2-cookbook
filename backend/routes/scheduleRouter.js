7b5271119fa24e5b580933bb5661bee747969151
Jennifer King <jenbellpgh@gmail.com>
2019-03-24 22:49:55 -0400

    Added skeleton for scheduleRouter.js

---
+++ backend/routes/scheduleRouter.js
@@ -0,0 +1,27 @@
+// Base requires:
+const express = require('express');
+const router = express.Router();
+
+// App requires/middleware
+const tags = require('../data/helpers/scheduleModel');
+
+
+/* ---------- Endpoints for /api/schedule ---------- */
+
+/* GET */
+
+/* GET schedule by ID */
+
+/* GET schedule by UserID */
+
+/* GET schedule by Date */
+
+/* PUT */
+
+/* POST */
+
+/* DELETE */
+
+
+/* ---------- Export ---------- */
+module.exports = router;
