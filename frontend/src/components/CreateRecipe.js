7618e47649cbd0ab1bb5570ffc3b28edcd4489ab
Robert Billington <chillingtonbillington@gmail.com>
2019-04-04 12:11:11 -0500

    Contains fully responsive NavBar, Landing Page, and Create Recipe pages

--- frontend/src/components/CreateRecipe.js
+++ frontend/src/components/CreateRecipe.js
@@ -21,38 +21,27 @@ class CreateRecipe extends React.Component{
       
       this.state = {
         recipeUrl: '',
-
-
         userId: '', //this.props.userId, ///Should I be doing this with auth id or retriving the user id?
-
+        tagModal: false,
         tag: null,
         recipeId:null, 
-        servings: ''
-       
+        servings: '',
+        calendarModal:false
       }
   }
-
-  //Can post and get by ID go in same function??
-//Post recipe to db
-//Get / Set recipe Id in state
-//Put id in post tag route
-
-//iFrame handled w front end
-
  componentDidMount(){
     const userId = localStorage.getItem('userId');
     this.setState({
          userId : userId
      });
  }
-s
+
  dropHandler = event =>{
       const url = event.dataTransfer.getData('text');
       console.log(url)
     this.setState({
         recipeUrl : url
     })
-     
 }
 postRecipe = (event) =>{
     event.preventDefault();
@@ -92,7 +81,6 @@ tagSelector = async(event) =>{
         
     })
     console.log(this.state.tag);
-    
 }
 addClass = (event) =>{
     event.target.classList.toggle('selected');
@@ -101,8 +89,6 @@ clickHandle = (event) =>{
     this.tagSelector(event);
     this.addClass(event);
 }
-
-
 postTagToRecipe = () =>{
     const tag = this.state.tag;
     console.log('I am tag from post',this.state.tag);
@@ -122,9 +108,29 @@ servingsAdjustor = async (event) =>{
     await this.setState({
         [event.target.name] : event.target.value
     });
-    console.log(this.state.servings);
-    
+    console.log(this.state.servings); 
 }
+openTagsMobile = () =>{
+    this.setState({
+        tagModal : true
+    })
+}
+closeTagsMobile = () =>{
+    this.setState({
+        tagModal : false
+    })
+}
+openCalendarMobile = () =>{
+    this.setState({
+        calendarModal: true
+    })
+}
+closeCalendarMobile = () =>{
+    this.setState({
+        calendarModal: false
+    })
+}
+
     render(){
         return(
             <div className='Create-Recipe'>
@@ -147,27 +153,27 @@ servingsAdjustor = async (event) =>{
                                 <h3 className='recipe-preview-header'>Slammin' Salmon</h3>
                                 <div className='recipe-info-container'>  
                                     <div className='prep-time-container'>
-                                        <p>Prep Time:</p>
+                                        <p className='prep-time'>Prep Time:</p>
                                         <p> 3 years</p>
                                     </div>
                                     <div className='cook-time-container'>
-                                        <p>Cook Time:</p>
+                                        <p className = 'cook-time'>Cook Time:</p>
                                         <p> 1 year</p>
                                     </div>
                                     <div className='servings-amount-container'>
-                                        <p>Servings:</p>
+                                        <p className ='servings-amount'>Servings:</p>
                                         <p> 4</p>
                                     </div>
-                                </div>      
-                            </div>
-                            <div className='recipe-preview-image'>
-                                <div className ='recipe-image-sub'>
+                                </div>
+                                <div className='recipe-preview-image'>
+                                    <div className ='recipe-image-sub'>
                                     <img className ='recipe-preview-img'src ='https://food.fnr.sndimg.com/content/dam/images/food/fullset/2013/11/25/0/FNK_pan-seared-salmon-with-kale-apple-salad_s4x3.jpg.rend.hgtvcom.616.462.suffix/1387918756116.jpeg' alt ='fish-dish' />
                                 </div> 
+                            </div>      
                             </div>
                             <div className='meal-tag-section'>
                                 <h3 className='meal-tag-header'>For which meal?</h3>
-                                <div className='meal-tag active focus'>
+                                <div className='meal-tag'>
                                     <p className='meal-tag-p' data-txt = 'breakfast' onClick={this.clickHandle}>Breakfast</p>
                                 
                                 </div>
@@ -188,6 +194,44 @@ servingsAdjustor = async (event) =>{
                                 
                                 </div>
                             </div>
+                            <div className='meal-tag-mobile'>
+                                <h3 className='meal-tag-header-mobile' onClick = {this.openTagsMobile}>Add meal tag</h3>
+                                    <div className= {this.state.tagModal ? 'meal-tag-modal-mobile-open' : 'meal-tag-modal-mobile-closed'}>
+                                        <div className='meal-tag-modal-sub'>
+                                            <div className='meal-tag-close' onClick={this.closeTagsMobile}>X</div>
+                                            <div className='meal-tag-mobile'>
+                                                <p className='meal-tag-p-mobile' data-txt = 'breakfast' onClick={this.clickHandle}>Breakfast</p>
+                                            
+                                            </div>
+                                            <div className='meal-tag-mobile'>
+                                                <p className='meal-tag-p-mobile' data-txt = 'lunch' onClick={this.clickHandle}>Lunch</p>
+                                            
+                                            </div>
+                                            <div className='meal-tag-mobile'>
+                                                <p className='meal-tag-p-mobile' data-txt = 'dinner' onClick={this.clickHandle}>Dinner</p>
+                                            
+                                            </div>
+                                            <div className='meal-tag-mobile'>
+                                                <p className='meal-tag-p-mobile' data-txt = 'snack' onClick={this.clickHandle}>Dessert</p>
+                                            
+                                            </div>
+                                            <div className='meal-tag-mobile'>
+                                                <p className='meal-tag-p-mobile' data-txt = 'dessert' onClick={this.clickHandle}>Snack</p>
+                                            
+                                            </div>
+                                        </div>    
+                                    </div>
+                            </div>
+                            <div className='calendar-mobile'>
+                                <h3  className = 'calendar-modal-click-header' onClick={this.openCalendarMobile}>Add recipe to calendar</h3>
+                                <div className={this.state.calendarModal ? 'calendar-mobile-open' : 'calendar-mobile-closed'}>
+                                    <div className= 'mobile-calendar-modal'>
+                                        <div onClick = {this.closeCalendarMobile}className='close-mobile-calendar'>X</div>
+                                        <Calendar calendarType = {'US'} onClickDay = {this.dayClick}/>
+                                    </div>    
+                                </div>    
+                            </div>
+                            <div className='add-recipe-button-mobile' onClick={this.postTagToRecipe}> Save </div>
                         </div>     
                         <div className='recipe-modification-section'>
                             <div className='calendar-section'>
