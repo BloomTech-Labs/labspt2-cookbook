9e7f11b945db53e2f811d3244be059993ae37ff2
Robert Billington <chillingtonbillington@gmail.com>
2019-04-04 19:27:41 -0500

    Working responsive design/most styling for RecipeList page

--- frontend/src/components/RecipeList.js
+++ frontend/src/components/RecipeList.js
@@ -91,87 +91,74 @@ onChangeDate = (event) =>{
     render(){
         return (
              <div className="Recipe-List-Page">
-             <NavBar />
-
-            <div className="recipe-list-container">
-
-            <div className='recipe-list-intro-container'>
+                <NavBar/>
+                <div className="recipe-list-container">
+                    <div className='recipe-list-sub-container'>   
+                        <div className='recipe-list-intro-container'>
                             <h1 className='recipe-list-header'>Your Recipes!</h1>
                             <div className='recipe-list-p'>View and search your collection of recipes, view single recipes and modify for later use by your shopping list.</div>
-            </div>
-            <div className="recipe-list-search-section" >
-                            
-                        
-                            <Link to="/create" className="create-recipe-link">Add Recipe</Link>
-
-                        <div className="search-bar">Search your recipes<input className="search-bar" /></div>
-                        <div onClick={this.filterModalOpen} className='filter-button'>Filter</div>
-                        
-                        <div className= {this.state.filterModal ? 'filter-modal-open' : 'filter-modal-closed'}>
-                            <div onClick={this.filterModalClose}>X</div>
-                            <div className='filter-modal-header'>Filter by my type</div>
-                            <div className='filter-modal-inputs-container'>
-                                <div>Breakfast</div>
-                                <div>Lunch</div>
-                                <div>Dinner</div>
-                                <div>Snack</div>
-                                <div>Dessert</div>
+                        </div>
+                        <div className="recipe-list-search-section" >
+                            <div className='recipe-list-button-container'>
+                                <Link to="/create" className="create-recipe-link">Add Recipe</Link>
+                                <div onClick={this.filterModalOpen} className='filter-button'>Filter</div>
+                            </div>    
+                            <input placeholder ='  Search your recipes' className="search-bar-input" />
+                                <div className= {this.state.filterModal ? 'filter-modal-open' : 'filter-modal-closed'}>
+                                    <div className='filter-modal'>   
+                                        <div onClick={this.filterModalClose}>X</div>
+                                        <div className='filter-modal-header'>Filter by my type</div>
+                                        <div className='filter-modal-inputs-container'>
+                                            <div>Breakfast</div>
+                                            <div>Lunch</div>
+                                            <div>Dinner</div>
+                                            <div>Snack</div>
+                                            <div>Dessert</div>
+                                        </div>
+                                        <div className='filter-modal-submit-button'>Submit</div>
+                                    </div> 
+                                </div>
                             </div>
-                            <div className='filter-modal-submit-button'>Submit</div>
-                        </div> 
-
-                    </div>
-
-                
-                 
-                 <div className="recipeContent">
-
-
-
-                {this.props.recipes.map((item) => (
-                    <div key={item.name} style={{margin: '10px'}}>
-                    
-                        
-                        <Link to={`/recipes/${item.recipe_id}`} >
-                                    <div className='recipe-card-header'>{item.name}</div>
-                                    <div className='recipe-card-content'>text some card text</div>
-                        </Link>
-                        
-                        <div className='recipe-card-button-container'>
+                        <div className="recipe-list">
+                            {this.props.recipes.map((item) => (
+                            <div key={item.name} className='recipe-card'>
+                                <Link to={`/recipes/${item.recipe_id}`} >
+                                            <div className='recipe-card-header'>{item.name}</div>
+                                            <div className='recipe-card-content'>text some card text</div>
+                                </Link>
+                                <div className='recipe-card-button-container'>
                                     <div onClick={() => this.editModalOpen(item)} className='recipe-card-edit-button'>Edit</div>
                                     <div className="recipe-card-delete-button">Delete</div>
-                        </div>
-                    
-                    
-                    
-                    
-                    
-                    </div>
-                )   )}
-
-                        <div className={this.state.editModal ? 'recipe-edit-modal-open' : 'recipe-edit-modal-closed'}>
-                            <div className ='recipe=edit-modal-header'>Edit {this.state.selectedItem.name}</div>
-                            <form className='edit-recipe-form'>
-                                <div onClick ={this.editModalClose}>X</div>
-                                <div>
-                                    <p>Select date</p>
-                                    <input type = 'date' value ={this.state.dateChange} onChange = {this.onChangeDate}/>
                                 </div>
-                                <div className='edit-modal-inputs-container'>
-                                    <div>Breakfast</div>
-                                    <div>Lunch</div>
-                                    <div>Dinner</div>
-                                    <div>Snack</div>
-                                    <div>Dessert</div>
-                                </div>
-                                <div className ='edit-modal-submit-button'>Submit</div>
-                            </form>
-                        </div>
+                            </div>
+                        )   )}
 
-                
-                </div>
-            </div>   
-        </div>          
+                                <div className={this.state.editModal ? 'recipe-edit-modal-open' : 'recipe-edit-modal-closed'}>
+                                    <div className ='recipe-edit-modal-header'>Edit {this.state.selectedItem.name}</div>
+                                    <div className='recipe-edit-modal'>   
+                                        <form className='edit-recipe-form'>
+                                            <div onClick ={this.editModalClose}>X</div>
+                                            <div>
+                                                <p>Select date</p>
+                                                <input type = 'date' value ={this.state.dateChange} onChange = {this.onChangeDate}/>
+                                            </div>
+                                            <div className='edit-modal-inputs-container'>
+                                                <div>Breakfast</div>
+                                                <div>Lunch</div>
+                                                <div>Dinner</div>
+                                                <div>Snack</div>
+                                                <div>Dessert</div>
+                                            </div>
+                                            <div className ='edit-modal-submit-button'>Submit</div>
+                                        </form>
+                                    </div>     
+                                </div>
+
+                        
+                        </div>
+                    </div>    
+                </div>   
+            </div>          
         )
     }
 } 
