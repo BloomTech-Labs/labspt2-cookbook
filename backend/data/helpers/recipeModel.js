bb2bf634ce2b4d7b52375bd88557993b2f7469c7
Jennifer King <jenbellpgh@gmail.com>
2019-05-01 03:19:37 -0400

    forcing non-null recipe_id

--- backend/data/helpers/recipeModel.js
+++ backend/data/helpers/recipeModel.js
@@ -110,6 +110,7 @@ module.exports = {
         return db('recipes')
           .transacting(trans)
           .insert({
+            recipe_id: 0,
             name: recipe.name,
             image: recipe.image,
             link: recipe.link
